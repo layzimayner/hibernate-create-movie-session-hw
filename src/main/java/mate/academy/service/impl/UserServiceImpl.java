ffbe4a9e78c67cb4aecf3c92368fc06d48a45746
layzimayner <vasyahunta@gmail.com>
2025-09-19 14:02:48 +0300

    check styling

--- src/main/java/mate/academy/service/impl/UserServiceImpl.java
+++ src/main/java/mate/academy/service/impl/UserServiceImpl.java
@@ -1,11 +1,10 @@
 package mate.academy.service.impl;
 
+import java.util.Optional;
 import mate.academy.dao.UserDao;
 import mate.academy.lib.Inject;
 import mate.academy.model.User;
 import mate.academy.service.UserService;
-
-import java.util.Optional;
 
 public class UserServiceImpl implements UserService {
     @Inject
